85863a3d33c672db21776d2d6d2cf2b0fa2975dd
Simran Preet Singh <simran5590@gmail.com>
2020-09-29 00:01:06 -0700

    Passing values to templates and from templates to snippets

--- personal/views.py
+++ personal/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_screen_view(request):
-    return render(request, "personal/home.html", {})
+    context = {
+        "some_string": "this is some string that I'm passing to the view",
+        "some_number": 45,
+        "list_of_values": [
+            "First entry",
+            "Second entry",
+            "Third entry",
+            "Fourth entry"
+        ]
+    }
+    return render(request, "personal/home.html", context)
