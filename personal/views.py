c0f6ccd436ea1f4b2dfed28e262ef5fe5288cfb3
Simran Preet Singh <simran5590@gmail.com>
2020-10-01 03:05:24 -0700

    Creating models, showing it in admin and showing it on a view

--- personal/views.py
+++ personal/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from personal.models import Question
+
+
 def home_screen_view(request):
     context = {
         "some_string": "this is some string that I'm passing to the view",
@@ -14,3 +17,10 @@ def home_screen_view(request):
         ]
     }
     return render(request, "personal/home.html", context)
+
+def questions(request):
+    questions = Question.objects.all()
+    context = {
+        "questions": questions
+    }
+    return render(request, "personal/questions.html", context)
