3824fb177482b87d0e52a8a217ba9384307cbff6
Simran Preet Singh <simran5590@gmail.com>
2020-10-02 05:22:57 -0700

    Creating customer user mode and removed old questions model and views and urls

--- MitchsBlog/settings.py
+++ MitchsBlog/settings.py
@@ -71,6 +71,8 @@ TEMPLATES = [
     },
 ]
 
+AUTH_USER_MODEL = 'account.Account'
+
 WSGI_APPLICATION = 'MitchsBlog.wsgi.application'
 
 # Database
--- personal/models.py
+++ personal/models.py
@@ -1,20 +1,2 @@
 from django.db import models
 
-PRIORITY = [
-    ("L", "Low"),
-    ("M", "Medium"),
-    ("H", "High")
-]
-
-
-class Question(models.Model):
-    title = models.CharField(max_length=60)
-    question = models.TextField(max_length=400)
-    priority = models.CharField(max_length=1, choices=PRIORITY)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = "Questions"
-        verbose_name_plural = "Questions"
